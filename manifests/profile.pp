3b3f6d2c08f2b78c4b608868eda663feeeb00b24
Samuel Keeley <sam.keeley@gmail.com>
2015-09-30 03:09:03 -0700

    Update profile.pp

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -3,9 +3,9 @@ class osx_setupassistant::profile{
 
 $disablesetupassistant  = $osx_setupassistant::disablesetupassistant
 
-mac_profiles_handler::manage { 'com.dropbox.corp.profiles.com.apple.SetupAssistant':
+mac_profiles_handler::manage { 'com.apple.SetupAssistant':
 	ensure  => $disablesetupassistant,
-	file_source => template('osx_setupassistant/com.dropbox.corp.profiles.com.apple.SetupAssistant.mobileconfig.erb'),
+	file_source => template('osx_setupassistant/com.apple.SetupAssistant.mobileconfig.erb'),
 	type => 'template',
 }
 
